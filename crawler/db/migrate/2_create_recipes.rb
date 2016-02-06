ce5ac071c3a611920136abe6b5cea29e044c2271
HASUMI Hitoshi <hasumi.hitoshi@tecnicasamba.com>
2016-02-06 13:23:58 +0900

    コメント修正

--- crawler/db/migrate/2_create_recipes.rb
+++ crawler/db/migrate/2_create_recipes.rb
@@ -4,7 +4,7 @@ class CreateRecipes < ActiveRecord::Migration
       t.references :menu, index: true
       t.integer    :cookpad_code,    comment: '例）http://cookpad.com/recipe/887445の"887445"'
       t.boolean    :image_available, comment: '画像取得に成功したらtrue、失敗したらfalse'
-      t.string     :image_file_name, comment: '画像のディレクトリ/ファイル名。data/images/[menus.rakuten_code]/[クックパッドのファイル名そのまま]'
+      t.string     :image_file_name, comment: '画像のディレクトリ/ファイル名。data/images/[menus.rakuten_code]/[この部分はGihubのイシューの指示に従うこと]'
 
       t.timestamps null: false
     end
