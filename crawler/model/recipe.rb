0f8eae9a6821971bc82adef0c38fdb78500e7f01
nagasef <nagasefmons@gmail.com>
2016-02-16 01:46:59 +0000

    画像取得方法の修正

--- crawler/model/recipe.rb
+++ crawler/model/recipe.rb
@@ -1,33 +1,34 @@
 
 def get_image(code,recipe_id)#HTMLファイルから画像の取得保存
-  image_url = Nokogiri::HTML.parse(File.open(File.expand_path("../../data/#{code}/#{recipe_id}.html", __FILE__)))
-  image_url = image_url.css('//img[data-track-label="Main Photo"]/@data-large-photo').to_s
-              
-  name = image_url.rpartition("/")
-  name = name[2].rpartition("?")
-  file_name = File.basename(name[0])
-  dir_name = File.expand_path("../../data/images/#{code}/", __FILE__)
-  file_path = dir_name + "/" + file_name
-            
-  FileUtils.mkdir_p(dir_name) unless FileTest.exist?(dir_name)
-
-  open(file_path, 'wb') do |output|
-    open(image_url) do |data|
-      output.write(data.read)
-    end
-  end
+  image_html = Nokogiri::HTML.parse(File.open(File.expand_path("../../data/#{code}/#{recipe_id}.html", __FILE__)))
+  image_url = image_html.css('//img[data-track-category="Recipe Large Photo"]/@data-large-photo').to_s
+  
+  unless image_url == ""
+    name = image_url.rpartition("/")
+    name = name[2].rpartition("?")
+    file_name = File.basename(name[0])
+    dir_name = File.expand_path("../../data/images/#{code}/", __FILE__)
+    file_path = dir_name + "/" + file_name
                 
-  image_available = FileTest.exist?(file_path)
-                    
-  #テーブルに挿入
-  Recipes.create(:cookpad_code => recipe_id,:image_available => image_available,:image_file_name => file_path)
-
+    FileUtils.mkdir_p(dir_name) unless FileTest.exist?(dir_name)
+    
+    open(file_path, 'wb') do |output|
+      open(image_url) do |data|
+        output.write(data.read)
+      end
+    end
+    
+    Recipes.create(:cookpad_code => recipe_id,:image_available => true,:image_file_name => file_path)
+  else
+    Recipes.create(:cookpad_code => recipe_id,:image_available => false,:image_file_name => nil)
+    File.unlink(File.expand_path("../../data/#{code}/#{recipe_id}.html", __FILE__)) 
+    puts "skip"
+  end
 end
 
-def get_html(page,code,recipe_id,recipe_url)#HTMLを保存する
-  p page.url.to_s
+def get_html(code,recipe_id,recipe_url)#HTMLを保存する
   unless Dir.exist?(File.expand_path("../../data/#{code}/", __FILE__))
-    mkdir(File.expand_path("../../data/#{code}/", __FILE__))
+    Dir.mkdir(File.expand_path("../../data/#{code}/", __FILE__))
   end
           
   open(recipe_url) do |uri|
