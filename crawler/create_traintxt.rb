2522c3fce2ca78fdf123506d6cd4dafd0b5b7c47
HASUMI Hitoshi <hitoshi.hasumi@monstar-lab.com>
2016-02-17 16:36:58 +0900

    add漏れ...

---
+++ crawler/create_traintxt.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require
+require 'uri'
+require './model/menu.rb'
+require './model/recipe.rb'
+require 'open-uri'
+require 'fileutils'
+
+File.open('./data/train.txt', 'w') do |traintxt|
+  Recipe.where(image_available: true).find_each do |recipe|
+    traintxt.puts "#{recipe.image_file_name} #{recipe.menu_id}"
+  end
+end
