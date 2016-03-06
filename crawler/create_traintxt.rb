d3905f163cb64462dee3010433503fef37adf4ac
Ubuntu <ubuntu@ip-172-31-31-191.ec2.internal>
2016-03-06 12:45:43 +0000

    60000files no.1

--- crawler/create_traintxt.rb
+++ crawler/create_traintxt.rb
@@ -5,6 +5,6 @@ require 'fileutils'
 
 File.open('./data/train.txt', 'w') do |traintxt|
   Recipe.where(image_available: true).find_each do |recipe|
-    traintxt.puts "#{recipe.image_file_name} #{recipe.menu_id}"
+    traintxt.puts "#{recipe.image_file_name.sub('ec2-user', 'ubuntu')} #{recipe.menu_id}"
   end
 end
